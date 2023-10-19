e2f927061d88cc085aa9402106f23e58f7223691
Luciano Kogut <ljkogut@hotmail.com>
2023-10-19 12:53:12 -0300

    feat: implementado endpoint de health check

---
+++ src/main/java/br/com/lucianokogut/todolist/controller/HealthCheckController.java
@@ -0,0 +1,23 @@
+package br.com.lucianokogut.todolist.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health-check")
+// http://localhost:8080/-------
+public class HealthCheckController {
+    @GetMapping("/")
+    public ResponseEntity<Object> getRetornoEntity() {
+        Map<String, String> retorno = new HashMap<>();
+        retorno.put("statusCode", "200");
+        retorno.put("resource", "apiToDoList");
+        return new ResponseEntity<>(retorno, HttpStatus.OK);
+    }
+}
