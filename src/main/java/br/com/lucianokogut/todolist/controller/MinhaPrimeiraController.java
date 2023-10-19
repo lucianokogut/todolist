50c2170f5e0623a4eb24a3a2bdfccacbe7ad3eee
Luciano Kogut <ljkogut@hotmail.com>
2023-10-19 12:57:05 -0300

    refact: ajustado retornos dos endpoints mockados

--- src/main/java/br/com/lucianokogut/todolist/controller/MinhaPrimeiraController.java
+++ src/main/java/br/com/lucianokogut/todolist/controller/MinhaPrimeiraController.java
@@ -1,11 +1,16 @@
 package br.com.lucianokogut.todolist.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/primeiraRota")
+@RequestMapping("/primeira-rota")
 // http://localhost:8080/-------
 public class MinhaPrimeiraController {
 
@@ -17,8 +22,24 @@ public class MinhaPrimeiraController {
      * PATCH - Alterar somente parte de um dado ou informação
      */
     // Método (funcionalidade) de uma classe
-    @GetMapping("/primeiroMetodo")
+    @GetMapping("/primeiro-metodo")
      public String primeiraMensagem() {
         return "Funcionou";
     }
+
+    @GetMapping("/data-mock")
+    public ResponseEntity<Object> getData() {
+        Map<String, String> data = new HashMap<>();
+        data.put("campoID", "1");
+        data.put("descricao", "Descrição do campo 1");
+        data.put("campoID", "2");
+        data.put("descrição", "Descrição do campo 2");
+        return new ResponseEntity<>(data, HttpStatus.OK);
+    }
+
+    @GetMapping("/data-string")
+    public String dataString() {
+        return "campoID: 1, descricao: Descrição do campo 1; campoID: 2, descricao: Descrição do campo 2";
+    }
+    
 }
