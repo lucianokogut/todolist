a462e36a10f869c2bc67c0682c5242e7af5c50bb
Luciano Kogut <ljkogut@hotmail.com>
2023-10-24 01:42:35 -0300

    feat: criado métodos getters e setters do UserModel

--- src/main/java/br/com/lucianokogut/todolist/user/UserModel.java
+++ src/main/java/br/com/lucianokogut/todolist/user/UserModel.java
@@ -2,7 +2,34 @@ package br.com.lucianokogut.todolist.user;
 
 public class UserModel {
 
-    public String username;
-    public String name;
-    public String password;
+    private String username;
+    private String name;
+    private String password;
+
+    /**
+     * Métodos para Get e Set (getters e setters)
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
