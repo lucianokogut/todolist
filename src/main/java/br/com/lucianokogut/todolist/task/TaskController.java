b50c167efd5367ee124174bd083512c83ce51bd9
Luciano Kogut <ljkogut@hotmail.com>
2023-10-29 18:46:31 -0300

    feat: indicado o ponto de chegada no controller

--- src/main/java/br/com/lucianokogut/todolist/task/TaskController.java
+++ src/main/java/br/com/lucianokogut/todolist/task/TaskController.java
@@ -15,6 +15,9 @@ public class TaskController {
     
     @PostMapping("/")
     public TaskModel create(@RequestBody TaskModel taskModel) {
+
+        System.out.println("Chegou no controller...");
+
         var task = this.taskRepository.save(taskModel);
         return task;
     }
