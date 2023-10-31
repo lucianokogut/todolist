91ed1880c55436c02682eecd46da1829ea8641d8
Luciano Kogut <ljkogut@hotmail.com>
2023-10-30 23:52:50 -0300

    docs: ajustado comentário sobre classe de interface

--- src/main/java/br/com/lucianokogut/todolist/user/IUserRepository.java
+++ src/main/java/br/com/lucianokogut/todolist/user/IUserRepository.java
@@ -5,7 +5,8 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
- * Criado a classe de interface que servirá de repositório padrão com os atributos da classe principal
+ * Criado a classe de interface que servirá de repositório padrão com os 
+ * atributos da classe principal
  */
 public interface IUserRepository extends JpaRepository<UserModel, UUID>{
     UserModel findByUsername(String username);
