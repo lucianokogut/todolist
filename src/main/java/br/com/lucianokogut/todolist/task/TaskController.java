10275e5933bfaabb3dd36e87723f98deefbd9cb3
Luciano Kogut <ljkogut@hotmail.com>
2023-11-01 01:29:12 -0300

    feat: implementado método para update das tasks

--- src/main/java/br/com/lucianokogut/todolist/task/TaskController.java
+++ src/main/java/br/com/lucianokogut/todolist/task/TaskController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -56,6 +58,18 @@ public class TaskController {
         var idUser = request.getAttribute("idUser");
         var tasks = this.taskRepository.findByIdUser((UUID) idUser);
         return tasks;
+    }
+
+    @PutMapping("/{id}")
+    public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+
+        var idUser = request.getAttribute("idUser");
+        taskModel.setIdUser((UUID) idUser);
+        taskModel.setId((UUID) id);
+        var task = this.taskRepository.save(taskModel);
+        //return ResponseEntity.status(HttpStatus.OK)
+        //        .body("A tarefa " + task + " \n foi inserida com sucesso!");
+        return task;
     }
 
 }
