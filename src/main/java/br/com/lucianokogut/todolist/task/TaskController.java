e0504a36b8f30f56366fe72ae56d3f4ea6215652
Luciano Kogut <ljkogut@hotmail.com>
2023-11-02 16:30:48 -0300

    refact: ajustado o retorno de TaskModel para Entity - incluído verificação para existência da tarefa - incluído verificação se tarefa selecionada é do usuário que a criou - incluído variável taskUpdated para enviá-la como retorno da Entity - incluído mensagem de log no console

--- src/main/java/br/com/lucianokogut/todolist/task/TaskController.java
+++ src/main/java/br/com/lucianokogut/todolist/task/TaskController.java
@@ -37,21 +37,25 @@ public class TaskController {
         var currentDate = LocalDateTime.now();
         /**
          * Verifica se a data de início da tarefa é válida (depois da data atual)
+         * e se a data de término é após a data de início (intervalo válido)
          */
         if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body("Na tarefa " + task
+                    .body("Na tarefa \n" + task
                             + " \n a data de início ou de término deve ser maior do que a data atual...");
         }
 
+        /**
+         * Verifica se a data de término é após a data de início (intervalo válido)
+         */
         if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body("A tarefa " + task.getId() + " com início em " + task.getStartAt() + " e término em "
+                    .body("A tarefa \n" + task.getId() + " com início em " + task.getStartAt() + " e término em "
                             + task.getEndAt() + " \n deve ter a data de início menor do que a data de término!");
         }
 
         return ResponseEntity.status(HttpStatus.OK)
-                .body("A tarefa " + task + " \n foi inserida com sucesso!");
+                .body("A tarefa \n" + task + " \n foi inserida com sucesso!");
     }
 
     @GetMapping("/")
@@ -62,16 +66,30 @@ public class TaskController {
     }
 
     @PutMapping("/{id}")
-    public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+    public ResponseEntity update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
 
         var idUser = request.getAttribute("idUser");
 
         var task = this.taskRepository.findById(id).orElse(null);
 
+        if (task == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Tarefa não encontrada!");
+        }
+
+        if (!task.getIdUser().equals(idUser)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Usuário " + idUser + " não tem permissão para alterar esta tarefa!");
+        }
+
         Utils.copyNonNullProperties(taskModel, task);
 
-        System.out.println("A tarefa " + task + " \n do usuário " + idUser + " foi alterada com sucesso!");
-        return this.taskRepository.save(task);
+        var taskUpdated = this.taskRepository.save(task);
+        System.out.println("A tarefa \n" + task + " \n do usuário " + idUser + " foi alterada de forma correta!");
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body("A tarefa \n" + task + " \n do usuário " + idUser + " foi alterada de forma correta!");
+
     }
 
 }
